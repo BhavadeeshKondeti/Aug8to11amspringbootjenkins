f79ae60d3c9060565c2c18760532349013c65bf6
KondetiBhavadeesh <bhavadeeshkondeti34@gmail.com>
2026-01-05 10:12:45 +0530

    springbootjenkins updated in local

--- src/main/java/com/excelr/AugJenkinsSpringbootapp8to11amApplication.java
+++ src/main/java/com/excelr/AugJenkinsSpringbootapp8to11amApplication.java
@@ -8,6 +8,8 @@ public class AugJenkinsSpringbootapp8to11amApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AugJenkinsSpringbootapp8to11amApplication.class, args);
+		
+		System.out.println("I done changes in springboot app");
 	}
 
 }
