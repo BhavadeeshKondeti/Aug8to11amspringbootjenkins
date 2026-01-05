49a0a22c32f0298fef11568798ac9b455d14fea2
KondetiBhavadeesh <bhavadeeshkondeti34@gmail.com>
2026-01-05 10:03:51 +0530

    SpringbootjenkinsApp added

---
+++ src/main/java/com/excelr/AugJenkinsSpringbootapp8to11amApplication.java
@@ -0,0 +1,13 @@
+package com.excelr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AugJenkinsSpringbootapp8to11amApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AugJenkinsSpringbootapp8to11amApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/excelr/AugJenkinsSpringbootapp8to11amApplicationTests.java
@@ -0,0 +1,13 @@
+package com.excelr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AugJenkinsSpringbootapp8to11amApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
